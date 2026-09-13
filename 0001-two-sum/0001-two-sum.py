37a78858ade5e18727b1499dd808efc06c2ca2c1
M Janeesha Reddy <mjaneeshareddy1503@gmail.com>
2026-09-13 11:00:39 +0530

    Time: 2145 ms (24.19%), Space: 13.4 MB (17.25%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -8,4 +8,15 @@ class Solution(object):
         for i in range(len(nums)):
             for j in range(i+1,len(nums)):
                 if nums[i]+nums[j]==target:
-                    return i,j
+                    return i,j         #O(n^2)
+        # left=0
+        # right=len(nums)-1
+        # while left<right:
+        #     sum=nums[left]+nums[right]
+        #     if sum==target:
+        #         return left,right
+        #     elif sum<target:
+        #         left+=1
+        #     else:
+        #         right-=1
+        # return[-1,-1]
