990d5b969e64a45f2b78da7d99077f3d6a90712d
M Janeesha Reddy <mjaneeshareddy1503@gmail.com>
2026-09-20 16:50:28 +0530

    Time: 232 ms (84.73%), Space: 17.1 MB (60.92%) - LeetHub

---
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def totalFruit(self, fruits):
+        """
+        :type fruits: List[int]
+        :rtype: int
+        """
+        left=0
+        freq={}
+        max_len=0
+        for right in range(len(fruits)):
+            freq[fruits[right]]=freq.get(fruits[right],0)+1
+            if len(freq)>2:
+                freq[fruits[left]]-=1
+                if freq[fruits[left]]==0:
+                    del freq[fruits[left]]
+                left+=1
+            max_len=max(max_len,right-left+1)
+        return max_len
